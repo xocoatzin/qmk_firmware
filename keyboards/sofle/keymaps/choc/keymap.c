875de234b8fda6d755166c7f179f8138c000d819
Alan Torres <atorresgomez@magicleap.com>
2022-04-29 11:58:41 +0200

    WIP

--- keyboards/sofle/keymaps/choc/keymap.c
+++ keyboards/sofle/keymaps/choc/keymap.c
@@ -162,22 +162,25 @@ led_config_t g_led_config = { {
   1, 1, 1, 1
 } };
 
-// void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//     if (get_highest_layer(layer_state) > 0) {
-//         uint8_t layer = get_highest_layer(layer_state);
-//
-//         for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-//             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-//                 uint8_t index = g_led_config.matrix_co[row][col];
-//
-//                 if (index >= led_min && index <= led_max && index != NO_LED &&
-//                 keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-//                     rgb_matrix_set_color(index, RGB_GREEN);
-//                 }
-//             }
-//         }
-//     }
-// }
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index <= led_max && index != NO_LED) {
+                    if(keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                        // rgb_matrix_set_color(index, RGB_GREEN);
+                    } else {
+                        rgb_matrix_set_color(index, RGB_OFF);
+                    }
+                }
+            }
+        }
+    }
+}
 
 #endif
 
